fcd1ac03a77f57ef78fdd4b362943491c409195e
Sanat <sanat.thukral@gmail.com>
2024-07-13 20:23:41 +0100

    fix: joke updated

--- src/widget/jokeGenerator/jokeGeneratorWidget.jsx
+++ src/widget/jokeGenerator/jokeGeneratorWidget.jsx
@@ -1,35 +1,67 @@
 // src/GeekJokeGenerator.js
-import React, { useState } from 'react';
-import { Box, Button, Text, Center, Spinner } from '@chakra-ui/react';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Text,
+  Center,
+  Spinner,
+  IconButton,
+} from "@chakra-ui/react";
+import { RepeatIcon } from "@chakra-ui/icons";
+import axios from "axios";
 
 const GeekJokeGenerator = () => {
-  const [joke, setJoke] = useState('');
+  const [joke, setJoke] = useState("");
   const [loading, setLoading] = useState(false);
 
   const fetchJoke = async () => {
     setLoading(true);
     try {
-      const response = await axios.get('https://geek-jokes.sameerkumar.website/api?format=json');
+      const response = await axios.get(
+        "https://geek-jokes.sameerkumar.website/api?format=json"
+      );
       setJoke(response.data.joke);
     } catch (error) {
-      setJoke('Failed to fetch a joke. Please try again.');
-      console.error('Error fetching joke:', error);
+      setJoke("Failed to fetch a joke. Please try again.");
+      console.error("Error fetching joke:", error);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Center height="100vh">
-      <Box textAlign="center" p="6" boxShadow="md" borderRadius="md" bg="white">
-        <Text fontSize="2xl" mb="4">Geek Joke Generator</Text>
-        <Button colorScheme="teal" onClick={fetchJoke} mb="4">
-          {loading ? <Spinner size="sm" /> : 'Generate Joke'}
+    <Center>
+      <Box textAlign="center" p="6" borderRadius="md" bg="white" maxH={"250px"}>
+        {!joke && (
+          <Text fontSize="2xl" mb="4">
+            Geek Joke Generator
+          </Text>
+        )}
+        <Button colorScheme="teal" onClick={fetchJoke} mb="4" hidden={!!joke}>
+          {loading ? <Spinner size="sm" /> : "Generate Joke"}
         </Button>
         {joke && (
-          <Box mt="4" p="4" bg="gray.100" borderRadius="md">
+          <Box
+            mt="4"
+            p="4"
+            borderRadius="md"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
             <Text fontSize="lg">{joke}</Text>
+            <IconButton
+              onClick={fetchJoke}
+              aria-label="Refresh joke"
+              icon={<RepeatIcon />}
+              variant="solid"
+              colorScheme="teal"
+              position="fixed"
+              bottom="16px"
+              right="16px"
+              zIndex="1000"
+            />
           </Box>
         )}
       </Box>
